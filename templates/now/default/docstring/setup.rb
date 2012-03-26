a5e1521b9ed9a86757eb1718b3bd8ed620bf57b7
Nikolai Weibull <now@bitwi.se>
2012-03-26 15:33:25 +0200

    Inline docstring_text

--- templates/now/default/docstring/setup.rb
+++ templates/now/default/docstring/setup.rb
@@ -4,7 +4,3 @@ def init
   return if object.docstring.blank? and not((object.respond_to? :is_alias? and object.is_alias?) or object.has_tag? :api)
   sections :text, T('tags')
 end
-
-def docstring_text
-  object.docstring.strip.empty? ? text_from_return(object) : object.docstring
-end
